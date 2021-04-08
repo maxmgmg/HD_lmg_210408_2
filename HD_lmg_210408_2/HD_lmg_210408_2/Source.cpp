8283d8b2be6b1d5a6fabe0969739ec717be37e0a
maxmgmg <joaunmg@knou.ac.kr>
2021-04-08 21:53:54 +0900

    loop-1 특별문제 2 풀이 완성

--- HD_lmg_210408_2/HD_lmg_210408_2/Source.cpp
+++ HD_lmg_210408_2/HD_lmg_210408_2/Source.cpp
@@ -1,4 +1,13 @@
 #include<Stdio.h>
 void main() {
-
+	for (int i = 0; i < 5; i++) {
+		for (int a = 4; a > i; a--) {
+			printf(" ");
+		}
+		for (int b = 0; b <= i; b++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	fgetc(stdin);
 }
